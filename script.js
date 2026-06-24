bc84479db141511fc8a62d858e274b847f7fdb09
jaiinderveer <jaiinderveersingh@gmail.com>
2026-06-24 23:44:47 +0530

    Generate diary cards dynamically with JavaScript

---
+++ script.js
@@ -0,0 +1,18 @@
+const diaryContainer = document.getElementById("diaryContainer");
+
+for(let day = 1; day <= 30; day++) {
+
+    const card = document.createElement("div");
+
+    card.classList.add("day-card");
+
+    card.innerHTML = `
+        <h3>Day ${day}</h3>
+        <p>Training entry</p>
+        <button class="day-btn">
+            View Details
+        </button>
+    `;
+
+    diaryContainer.appendChild(card);
+}
